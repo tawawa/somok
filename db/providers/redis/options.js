98e7dc002c62b9d6390a70e693b5ed32d31b7da4
Zulfiqar Ahmed <zamd@Zulfiqars-MacBook-Pro-2.local>
2016-09-08 03:05:44 +0100

    dont pass blank auth key

--- db/providers/redis/options.js
+++ db/providers/redis/options.js
@@ -1,5 +1,9 @@
-module.exports = {
+const options  = {
 	host:  process.env.REDIS_HOST,
-	port: process.env.REDIS_PORT || 6379,
-	auth_pass: process.env.REDIS_AUTH_KEY
-}
+	port: process.env.REDIS_PORT || 6379
+}; 
+
+if (process.env.REDIS_AUTH_KEY)
+	options.auth_pass = process.env.REDIS_AUTH_KEY;
+
+module.exports = options;
